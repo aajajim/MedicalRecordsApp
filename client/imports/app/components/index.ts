f025a55b93afa6624b69a2e3564bc34e9e893b21
aajaji <mohammed.aajaji.freelance@gmail.com>
2017-01-08 19:40:29 +0100

    Revert "Adding footer, but still issue"
    This reverts commit 5ad637f49355857059590524562b48796900ee8c.

--- client/imports/app/components/index.ts
+++ client/imports/app/components/index.ts
@@ -2,12 +2,10 @@
 import { MainPageComponent } from './main-page.component';
 import { ToolbarContentComponent } from './toolbar-content.component';
 import { SidenavContentComponent } from './sidenav-content.component';
-import { MainFooterComponent } from './main-footer.component';
 
 //Aggregated list of declarations
 export const APP_COMPONENTS_DECLARATIONS = [
     MainPageComponent,
-    MainFooterComponent,
     ToolbarContentComponent,
     SidenavContentComponent
 ];
