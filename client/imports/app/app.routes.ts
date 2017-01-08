08093fc405638a1bbaf446e4d7f2584cc78cd541
aajaji <mohammed.aajaji.freelance@gmail.com>
2017-01-08 19:31:56 +0100

    Adding footer, but still issue

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -6,5 +6,12 @@ import { MainPageComponent } from './components/main-page.component';
 
 //Exported routes
 export const APP_ROUTES_DECLARATIONS:Route[] = [
-    {path: '', component: MainPageComponent}
+    {path: '', component: MainPageComponent},
+    {path: 'help', component: MainPageComponent},
+    {path: 'about-us', component: MainPageComponent},
+    {path: 'our-team', component: MainPageComponent},
+    {path: 'persona-data', component: MainPageComponent},
+    {path: 'security', component: MainPageComponent},
+    {path: 'usage-conditions', component: MainPageComponent},
+    {path: 'router-link', component: MainPageComponent}
 ];
--- client/imports/app/components/index.ts
+++ client/imports/app/components/index.ts
@@ -2,10 +2,12 @@
 import { MainPageComponent } from './main-page.component';
 import { ToolbarContentComponent } from './toolbar-content.component';
 import { SidenavContentComponent } from './sidenav-content.component';
+import { MainFooterComponent } from './main-footer.component';
 
 //Aggregated list of declarations
 export const APP_COMPONENTS_DECLARATIONS = [
     MainPageComponent,
+    MainFooterComponent,
     ToolbarContentComponent,
     SidenavContentComponent
 ];
