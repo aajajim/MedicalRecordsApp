e71664bcec6d2773e87ee733279c0f78f610e251
aajaji <mohammed.aajaji.freelance@gmail.com>
2017-01-08 19:40:14 +0100

    Revert "Correct Footer issue"
    This reverts commit 0261d2e8a3459ed2316e0b8f20b527ef3d29311f.

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToolbarContentComponent } from './components/toolbar-content.component';
 
 import template from './app.component.html';
 import style from './app.component.scss';
